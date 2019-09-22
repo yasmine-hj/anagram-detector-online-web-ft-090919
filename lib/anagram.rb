4996a66654adc4df23b13f7e4e0608d7103d82d4
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-22 12:13:13 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  
+attr_reader :name
+
+def initialize(name)
+  @name = name
+end
+
+def match(arr)
+  arr.keep_if {|str| name.split('').sort == str.split('').sort}
+end
+  
+end
